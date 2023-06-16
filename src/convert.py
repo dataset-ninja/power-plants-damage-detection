a5aec49c10cc5b40c82364eef8cfc7673332b09a
almaz <almazgimaev@gmail.com>
2023-06-16 14:45:05 +0300

    add scripts

--- src/convert.py
+++ src/convert.py
@@ -1,15 +1,77 @@
-# Path to the original dataset
+# https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/GFYPQW
+
+import os
+import xml.etree.ElementTree as ET
 
 import supervisely as sly
+from supervisely.io.fs import dir_exists, get_file_ext, get_file_name
+from tqdm import tqdm
 
 
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    # * dataset_path = "Path to the local dir with dataset"
+    dataset_path = "/Users/almaz/Downloads/dataverse_files"
+    batch_size = 30
 
-    # Function should read local dataset and upload it to Supervisely project, then return project info.
+    images_ext = ".jpg"
+    ann_ext = ".xml"
 
-    raise NotImplementedError("The converter should be implemented manually.")
+    def _create_ann(image_path):
+        labels = []
 
-    # * return project
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
+
+        file_name = get_file_name(image_path)
+
+        ann_path = os.path.join(curr_data_path, file_name + ann_ext)
+
+        tree = ET.parse(ann_path)
+        root = tree.getroot()
+
+        coords_xml = root.findall(".//bndbox")
+        for curr_coord in coords_xml:
+            left = int(curr_coord[0].text)
+            top = int(curr_coord[1].text)
+            right = int(curr_coord[2].text)
+            bottom = int(curr_coord[3].text)
+
+            rect = sly.Rectangle(left=left, top=top, right=right, bottom=bottom)
+            label = sly.Label(rect, obj_class)
+            labels.append(label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+    all_data = os.listdir(dataset_path)
+
+    obj_class = sly.ObjClass("damage", sly.Rectangle)
+    project = api.project.create(workspace_id, project_name)
+    meta = sly.ProjectMeta(obj_classes=[obj_class])
+    api.project.update_meta(project.id, meta.to_json())
+
+    for curr_folder in all_data:
+        curr_data_path = os.path.join(dataset_path, curr_folder)
+        if dir_exists(curr_data_path):
+            dataset = api.dataset.create(project.id, curr_folder)
+
+            images_names = [
+                item for item in os.listdir(curr_data_path) if get_file_ext(item) == images_ext
+            ]
+
+            progress = tqdm(total=len(images_names), desc="Create dataset {}".format(curr_folder))
+
+            for images_names_batch in sly.batched(images_names, batch_size=batch_size):
+                img_pathes_batch = [
+                    os.path.join(curr_data_path, image_name) for image_name in images_names_batch
+                ]
+
+                img_infos = api.image.upload_paths(dataset.id, images_names_batch, img_pathes_batch)
+                img_ids = [im_info.id for im_info in img_infos]
+
+                anns = [_create_ann(image_path) for image_path in img_pathes_batch]
+                api.annotation.upload_anns(img_ids, anns)
+
+                progress.update(len(images_names_batch))
+    return project
--- src/settings.py
+++ src/settings.py
@@ -5,38 +5,44 @@ from dataset_tools.templates import AnnotationType, CVTask, Industry, License
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Damage Detection of Power Plants"
+PROJECT_NAME_FULL: str = "Replication Data for Remote Damage Detection of Power Plants using Deep Learning based drone image analysis"
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-INDUSTRIES: List[Industry] = None
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+LICENSE: License = License.CC0_1_0()
+INDUSTRIES: List[Industry] = [Industry.Industrial()]
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [CVTask.ObjectDetection()]
 
-RELEASE_YEAR: int = None
-HOMEPAGE_URL: str = None
+RELEASE_YEAR: int = 2020
+HOMEPAGE_URL: str = (
+    "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/GFYPQW"
+)
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 1040
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/power-plants-damage-detection"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[
+    Union[str, dict]
+] = "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/GFYPQW#"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
 PAPER: Optional[str] = None
-CITATION_URL: Optional[str] = None
+CITATION_URL: Optional[
+    str
+] = "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/GFYPQW"
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
 TAGS: List[str] = None
