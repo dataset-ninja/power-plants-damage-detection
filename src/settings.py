cfbaddba77f33627154801a021c303a8ea04139e
almaz <almazgimaev@gmail.com>
2023-06-16 17:16:29 +0300

    image id

--- src/settings.py
+++ src/settings.py
@@ -22,7 +22,7 @@ HOMEPAGE_URL: str = (
 )
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 1040
+PREVIEW_IMAGE_ID: int = 585678
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/power-plants-damage-detection"
