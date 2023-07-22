a144f89ae5da23be444b1093a6c293d40b12141b
Ars Vladimirov <grokhael@gmail.com>
2023-07-22 12:37:35 +0600

    upd custom data

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,9 +1,7 @@
-**Replication Data for Remote Damage Detection of Power Plants using Deep Learning based drone image analysis** is a dataset for object detection tasks. It is used in the industrial industry.
+**Replication Data for Remote Damage Detection of Power Plants Using Deep Learning Based Drone Image Analysis** is a dataset for object detection tasks. It is used in the drone inspection and damage detection domains, and engineering research. Possible applications of the dataset could be in the energy industry. 
 
 The dataset consists of 1136 images with 1136 labeled objects belonging to 1 single class (*damage*).
 
-Images in the Damage Detection of Power Plants dataset has bounding box annotations. All images are labeled (i.e. with annotations). There are 4 splits in the dataset: *Solar_large* (122 images), *Solar_small* (516 images), *Solar_small_IR* (67 images), and *Wind* (431 images). The dataset was released in 2020.
+Images in the Damage Detection of Power Plants dataset have bounding box annotations. All images are labeled (i.e. with annotations). There are 4 splits in the dataset: *Solar_large* (122 images), *Solar_small* (516 images), *Solar_small_IR* (67 images), and *Wind* (431 images). The dataset was released in 2020 by the [Green University of Bangladesh](https://green.edu.bd/) and [University of Liberal Arts Bangladesh](https://ulab.edu.bd/).
 
-Here is the visualized example grid with annotations:
-
-<img src="https://github.com/dataset-ninja/power-plants-damage-detection/raw/main/visualizations/horizontal_grid.png">
+<img src="https://github.com/dataset-ninja/power-plants-damage-detection/raw/main/visualizations/poster.png">
--- src/settings.py
+++ src/settings.py
@@ -1,22 +1,40 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
 ##################################
 PROJECT_NAME: str = "Damage Detection of Power Plants"
-PROJECT_NAME_FULL: str = "Replication Data for Remote Damage Detection of Power Plants using Deep Learning based drone image analysis"
+PROJECT_NAME_FULL: str = "Replication Data for Remote Damage Detection of Power Plants Using Deep Learning Based Drone Image Analysis"
 
 ##################################
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.CC0_1_0()
-INDUSTRIES: List[Industry] = [Industry.Industrial()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [
+    Industry.Energy(is_used=False),
+    Domain.DroneInspection(),
+    Domain.DamageDetection(),
+    Research.Engineering(),
+]
+CATEGORY: Category = Category.EnergyAndUtilities()
+
 CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
 ANNOTATION_TYPES: List[AnnotationType] = [CVTask.ObjectDetection()]
 
-RELEASE_YEAR: int = 2020
+RELEASE_DATE: Optional[str] = "2020-10-24"  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = None
+
 HOMEPAGE_URL: str = (
     "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/GFYPQW"
 )
@@ -31,20 +49,41 @@ GITHUB_URL: str = "https://github.com/dataset-ninja/power-plants-damage-detectio
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[
-    Union[str, dict]
-] = "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/GFYPQW#"
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = {
+    "Solar_large.rar": "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/GFYPQW#",
+    "Solar_small.rar": "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/GFYPQW#",
+    "Solar_small_IR.rar": "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/GFYPQW#",
+    "Wind.rar": "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/GFYPQW#",
+}
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
-PAPER: Optional[str] = None
+PAPER: Optional[str] = "https://www.sciencedirect.com/science/article/pii/S2352484721005102"
 CITATION_URL: Optional[
     str
 ] = "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/GFYPQW"
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+AUTHORS: Optional[List[str]] = [
+    "Shihavuddin, ASM",
+    "Mohammad Rifat Ahmmad Rashid",
+    "Xiao Chen",
+    "Md Hasan Maruf",
+    "Mohammad Asif UL Haq",
+    "Muhammad Abul Hasan",
+    "Ahmed Al Mansur",
+]
+
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
+    "Green University of Bangladesh",
+    "University of Liberal Arts Bangladesh",
+]
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = [
+    "https://green.edu.bd/",
+    "https://ulab.edu.bd/",
+]
+
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
 TAGS: List[str] = None
 
 ##################################
@@ -59,10 +98,15 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
-        "industries": INDUSTRIES,
+        "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -74,13 +118,16 @@ def get_settings():
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
+    settings["release_date"] = RELEASE_DATE
     settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
 
     return settings
